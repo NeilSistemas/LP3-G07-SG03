71ae534e959ba19e4670426c78347f0cc7de8acc
Neil Linares <76920688@ucsm.edu.pe>
2023-08-20 14:31:31 -0500

    Clase Principal con Codigo java

---
+++ PyLab01.java
@@ -0,0 +1,20 @@
+public class Principal {
+    public static void main(String[] args) {
+        int vida, rp;
+        boolean fl = false;
+        vida = 0;
+        scanner sc = new Scanner(System.in);
+        do {
+            vida ++;
+            System.out.println("vida:"+ vida + "\t¿Cual es el numero secreto?");
+            rp = sc.nextInt();
+            if (rp == 1234)
+                fl = true;
+
+        }while(vida < 3 && !fl);
+        if (fl == true)
+            System.out.println("Adivinaste!!!!! ");
+        else
+            System.out.println("ups, perdiste...");
+    }
+}
