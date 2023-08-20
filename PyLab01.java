160e7c0dfc1c435eeeca6e0a08414b98bdef4d4c
Neil Linares <76920688@ucsm.edu.pe>
2023-08-20 15:54:57 -0500

    Primer cambio a Clase Principal con codigo java

--- PyLab01.java
+++ PyLab01.java
@@ -1,23 +1,28 @@
 public class Principal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa el número secreto: ");
+        int num = sc.nextInt();
+        
         int vida, rp;
         boolean fl = false;
         vida = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Ingresa un numero: ");
-        rp = sc.nextInt();
         do {
             vida++;
+            System.out.println("Vida:" + vida + "\t¿Cuál es el número?");
             rp = sc.nextInt();
-            if (rp == 1234)
+            if (rp == num) {
                 fl = true;
+                break;
+            }
 
         } while (vida < 3 && !fl);
         
-        if (fl)
-            System.out.println("Adivinaste!!!!! ");
-        else
-            System.out.println("Ups, perdiste...");
+        if (fl) {
+            System.out.println("¡Adivinaste el número secreto!");
+        } else {
+            System.out.println("Ups, perdiste. El número secreto era: " + numeroSecreto);
+        }
         
         sc.close();
     }
